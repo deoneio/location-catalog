e68b310281828aa56ed7b38d3d2f62c215c4f74b
Audi Asmarandana <audi.asmarandana@gdn-commerce.com>
2026-07-18 18:26:14 +0700

    feat: configure proxy to directus API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/api/**': { proxy: (process.env.DIRECTUS_URL || 'http://10.169.6.124:8055') + '/**' }
+  }
 })
