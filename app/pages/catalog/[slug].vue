d10b90080cc3230a5af59a8c597c9ac83ce2c20f
Audi Asmarandana <audi.asmarandana@gdn-commerce.com>
2026-07-18 18:15:28 +0700

    feat: add catalog detail placeholder page

---
+++ app/pages/catalog/[slug].vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Location Detail</h1>
+    <p>This is a placeholder for the individual location detail page.</p>
+    <p>Slug parameter: {{ $route.params.slug }}</p>
+  </div>
+</template>
